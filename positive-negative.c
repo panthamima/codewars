8f42742ad8544b54f6358284217dc43e3d6b08d5
panthamima <nastyadima2003@mail.ru>
2022-01-03 22:43:45 +0700

    my first prgm in codewars

---
+++ positive-negative.c
@@ -0,0 +1,15 @@
+int main(int num) {
+  //makeNegative
+
+    if(num > 0) {
+        num = num - (num * 2);
+        return num;
+    }
+
+    else if(num < 0) {
+        return num; 
+    }
+    else {
+        return num;
+    }
+}
